c1620e3b54abe8c8751e763f1c47fd6f85fbbe0e
mangebange76 <magnusberg47@gmail.com>
2025-07-23 08:27:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -9,11 +9,35 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 credentials = Credentials.from_service_account_info(st.secrets["GOOGLE_CREDENTIALS"], scopes=scope)
 gc = gspread.authorize(credentials)
 
-# Öppna kalkylarket
+# Spreadsheet-URL
 SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1--mqpIEEta9An4kFvHZBJoFlRz1EtozxCy2PnD4PNJ0/edit?usp=drivesdk"
 sh = gc.open_by_url(SPREADSHEET_URL)
 
+# Kolumner
+DATA_COLUMNS = [
+    "Datum", "Typ", "DP", "DPP", "DAP", "TPA", "TPP", "TAP",
+    "Enkel vaginal", "Enkel anal",
+    "Kompisar", "Pappans vänner", "Nils vänner", "Nils familj", "Övriga män",
+    "Älskar med", "Sover med", "Nils sex",
+    "DT tid per man (sek)", "DT total tid (sek)", "Total tid (sek)", "Total tid (h)",
+    "Scenens längd (h)",
+    "Prenumeranter", "Intäkt ($)", "Kvinnans lön ($)", "Mäns lön ($)", "Kompisars lön ($)",
+    "Minuter per kille"
+]
+
 INST_COLUMNS = ["Inställning", "Värde", "Senast ändrad"]
+
+def init_sheet(name, cols):
+    try:
+        worksheet = sh.worksheet(name)
+    except:
+        worksheet = sh.add_worksheet(title=name, rows="1000", cols="30")
+        worksheet.update("A1", [cols])
+    else:
+        existing_cols = worksheet.row_values(1)
+        if existing_cols != cols:
+            worksheet.resize(rows=1)
+            worksheet.update("A1", [cols])
 
 def init_inställningar():
     try:
@@ -22,16 +46,39 @@ def init_inställningar():
         worksheet = sh.add_worksheet(title="Inställningar", rows="100", cols="3")
         worksheet.update("A1", [INST_COLUMNS])
         standard = [
+            ["Tid per man (minuter)", "2", datetime.now().strftime("%Y-%m-%d")],
             ["Kvinnans namn", "Malin", datetime.now().strftime("%Y-%m-%d")],
             ["Födelsedatum", "1984-03-26", datetime.now().strftime("%Y-%m-%d")],
             ["Startdatum", "2014-03-26", datetime.now().strftime("%Y-%m-%d")],
             ["Kompisar", "100", datetime.now().strftime("%Y-%m-%d")],
             ["Pappans vänner", "40", datetime.now().strftime("%Y-%m-%d")],
             ["Nils vänner", "30", datetime.now().strftime("%Y-%m-%d")],
-            ["Nils familj", "20", datetime.now().strftime("%Y-%m-%d")],
-            ["DT tid per man (sek)", "15", datetime.now().strftime("%Y-%m-%d")]
+            ["Nils familj", "20", datetime.now().strftime("%Y-%m-%d")]
         ]
         worksheet.update(f"A2:C{len(standard)+1}", standard)
+
+def säkerställ_kolumner(df):
+    for kolumn in DATA_COLUMNS:
+        if kolumn not in df.columns:
+            df[kolumn] = ""
+    return df[DATA_COLUMNS]
+
+def spara_data(df):
+    df = df.fillna("").astype(str)
+    worksheet = sh.worksheet("Data")
+    worksheet.clear()
+    worksheet.update("A1", [df.columns.tolist()] + df.values.tolist())
+
+def ladda_data():
+    try:
+        worksheet = sh.worksheet("Data")
+        data = worksheet.get_all_records()
+        if not data:
+            return pd.DataFrame(columns=DATA_COLUMNS)
+        df = pd.DataFrame(data)
+        return säkerställ_kolumner(df)
+    except:
+        return pd.DataFrame(columns=DATA_COLUMNS)
 
 def läs_inställningar():
     worksheet = sh.worksheet("Inställningar")
@@ -56,23 +103,3 @@ def spara_inställning(nyckel, värde):
         worksheet.update_cell(idx + 2, 3, idag)
     else:
         worksheet.append_row([nyckel, str(värde), idag])
-
-# Initiera inställningar om de saknas
-init_inställningar()
-inst = läs_inställningar()
-
-# Sidopanel
-with st.sidebar:
-    st.header("Inställningar")
-    namn = st.text_input("Kvinnans namn", value=str(inst.get("Kvinnans namn", "")))
-    född = st.date_input("Födelsedatum", value=pd.to_datetime(inst.get("Födelsedatum", "1984-03-26")))
-    startdatum = st.date_input("Startdatum (första scen)", value=pd.to_datetime(inst.get("Startdatum", "2014-03-26")))
-
-    spara_inställning("Kvinnans namn", namn)
-    spara_inställning("Födelsedatum", född.strftime("%Y-%m-%d"))
-    spara_inställning("Startdatum", startdatum.strftime("%Y-%m-%d"))
-
-    st.divider()
-    for fält in ["Kompisar", "Pappans vänner", "Nils vänner", "Nils familj", "DT tid per man (sek)"]:
-        val = st.number_input(fält, value=float(inst.get(fält, 0)), min_value=0.0, step=1.0)
-        spara_inställning(fält, val)
