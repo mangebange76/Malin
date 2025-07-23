08867034929018d1fa3cac008ad8e1c6ea420006
mangebange76 <magnusberg47@gmail.com>
2025-07-23 19:57:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,101 @@
-# app.py – Del 1
-
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 import gspread
 from google.oauth2.service_account import Credentials
-from random import sample, shuffle
+from random import sample
+import math
 
-# Autentisering
+# Autentisering och kalkylarks-URL
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 credentials = Credentials.from_service_account_info(st.secrets["GOOGLE_CREDENTIALS"], scopes=scope)
 gc = gspread.authorize(credentials)
-
-# Spreadsheet
 SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1--mqpIEEta9An4kFvHZBJoFlRz1EtozxCy2PnD4PNJ0/edit?usp=drivesdk"
 sh = gc.open_by_url(SPREADSHEET_URL)
 
-# Kolumner
-DATA_COLUMNS = [
-    "Datum", "Typ", "DP", "DPP", "DAP", "TPA", "TPP", "TAP",
-    "Enkel vaginal", "Enkel anal",
+# Rubriker / kolumnstruktur
+RUBRIKER = [
+    "Datum", "Typ", "DP", "DPP", "DAP",
+    "TPA", "TPP", "TAP", "Enkel vaginal", "Enkel anal",
     "Kompisar", "Pappans vänner", "Nils vänner", "Nils familj", "Övriga män",
-    "Älskar med", "Sover med", "Nils sex",
-    "DT tid per man (sek)", "DT total tid (sek)", "Total tid (sek)", "Total tid (h)",
-    "Prenumeranter", "Intäkt ($)", "Kvinnans lön ($)", "Mäns lön ($)", "Kompisars lön ($)",
-    "Minuter per kille", "Scenens längd (h)"
+    "Älskar med", "Sover med", "Nils sex", "DT tid per man (sek)",
+    "DT total tid (sek)", "Total tid (sek)", "Total tid (h)",
+    "Prenumeranter", "Intäkt ($)", "Kvinnans lön ($)", "Mäns lön ($)",
+    "Kompisars lön ($)", "Minuter per kille", "Scenens längd (h)"
 ]
 
-INST_COLUMNS = ["Inställning", "Värde", "Senast ändrad"]
+INST_KOL = ["Inställning", "Värde", "Senast ändrad"]
 
+# Initiera blad om de inte finns
 def init_sheet(name, cols):
     try:
         worksheet = sh.worksheet(name)
     except:
-        worksheet = sh.add_worksheet(title=name, rows="1000", cols="30")
+        worksheet = sh.add_worksheet(title=name, rows="1000", cols="50")
         worksheet.update("A1", [cols])
     else:
-        existing_cols = worksheet.row_values(1)
-        if existing_cols != cols:
-            worksheet.resize(rows=1)
+        if worksheet.row_values(1) != cols:
+            worksheet.clear()
             worksheet.update("A1", [cols])
+
+def init_inställningar():
+    try:
+        worksheet = sh.worksheet("Inställningar")
+    except:
+        worksheet = sh.add_worksheet(title="Inställningar", rows="100", cols="3")
+        worksheet.update("A1", [INST_KOL])
+        standard = [
+            ["Kvinnans namn", "Malin", datetime.now().strftime("%Y-%m-%d")],
+            ["Födelsedatum", "1984-03-26", datetime.now().strftime("%Y-%m-%d")],
+            ["Startdatum", "2014-03-26", datetime.now().strftime("%Y-%m-%d")],
+            ["Kompisar", "100", datetime.now().strftime("%Y-%m-%d")],
+            ["Pappans vänner", "40", datetime.now().strftime("%Y-%m-%d")],
+            ["Nils vänner", "30", datetime.now().strftime("%Y-%m-%d")],
+            ["Nils familj", "20", datetime.now().strftime("%Y-%m-%d")]
+        ]
+        worksheet.update("A2:C8", standard)
+
+def läs_inställningar():
+    worksheet = sh.worksheet("Inställningar")
+    data = worksheet.get_all_records()
+    inst = {}
+    for rad in data:
+        val = str(rad["Värde"])
+        try:
+            inst[rad["Inställning"]] = float(val.replace(",", "."))
+        except:
+            inst[rad["Inställning"]] = val
+    return inst
+
+def spara_inställning(nyckel, värde):
+    worksheet = sh.worksheet("Inställningar")
+    df = pd.DataFrame(worksheet.get_all_records())
+    idag = datetime.now().strftime("%Y-%m-%d")
+    if nyckel in df["Inställning"].values:
+        rad = df[df["Inställning"] == nyckel].index[0] + 2
+        worksheet.update_cell(rad, 2, str(värde))
+        worksheet.update_cell(rad, 3, idag)
+    else:
+        worksheet.append_row([nyckel, str(värde), idag])
+
+def spara_data(df):
+    worksheet = sh.worksheet("Data")
+    worksheet.clear()
+    worksheet.update("A1", [df.columns.tolist()] + df.fillna("").astype(str).values.tolist())
+
+def load_data():
+    try:
+        worksheet = sh.worksheet("Data")
+        data = worksheet.get_all_records()
+        if not data:
+            return pd.DataFrame(columns=RUBRIKER)
+        df = pd.DataFrame(data)
+        for col in RUBRIKER:
+            if col not in df.columns:
+                df[col] = ""
+        return df[RUBRIKER]
+    except:
+        return pd.DataFrame(columns=RUBRIKER)
 
 # app.py – Del 2
 
