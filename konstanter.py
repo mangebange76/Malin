ebe5f99d9d970a6dd68ddb67ecad4fd52324a57b
mangebange76 <magnusberg47@gmail.com>
2025-07-24 11:50:50 +0300

    Create konstanter.py

---
+++ konstanter.py
@@ -0,0 +1,16 @@
+# konstanter.py
+
+COLUMNS = [
+    "Datum", "Typ", "Scenens längd (h)", "Antal vilodagar", "Övriga män",
+    "Enkel vaginal", "Enkel anal", "DP", "DPP", "DAP", "TPP", "TPA", "TAP",
+    "Kompisar", "Pappans vänner", "Nils vänner", "Nils familj",
+    "DT tid per man (sek)", "Älskar med", "Sover med", "Nils sex",
+    "Prenumeranter", "Intäkt ($)", "Kvinnans lön ($)", "Mäns lön ($)", "Kompisars lön ($)",
+    "DT total tid (sek)", "Total tid (sek)", "Total tid (h)", "Minuter per kille"
+]
+
+def säkerställ_kolumner(df):
+    for col in COLUMNS:
+        if col not in df.columns:
+            df[col] = 0
+    return df[COLUMNS]
