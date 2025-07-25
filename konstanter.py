bd76e128e05ea92b52e3bbdc2e8d24a8d37cc101
mangebange76 <magnusberg47@gmail.com>
2025-07-25 10:12:55 +0300

    Update konstanter.py

--- konstanter.py
+++ konstanter.py
@@ -1,43 +1,14 @@
 COLUMNS = [
-    "Datum",
-    "Typ",
-    "Antal vilodagar",
-    "Nya män",
-    "Enkel vaginal",
-    "Enkel anal",
-    "DP",
-    "DPP",
-    "DAP",
-    "TPP",
-    "TPA",
-    "TAP",
-    "Tid enkel",
-    "Tid dubbel",
-    "Tid trippel",
-    "Vila",
-    "Kompisar",
-    "Pappans vänner",
-    "Nils vänner",
-    "Nils familj",
-    "DT tid per man",
-    "Antal varv",
-    "Älskar med",
-    "Sover med",
-    "Nils sex",
-    "Prenumeranter",
-    "Intäkt ($)",
-    "Kvinnans lön ($)",
-    "Mäns lön ($)",
-    "Kompisars lön ($)",
-    "DT total tid (sek)",
-    "Total tid (sek)",
-    "Total tid (h)",
-    "Minuter per kille"
+    "Datum", "Typ", "Antal vilodagar", "Nya män", "Enkel vaginal", "Enkel anal",
+    "DP", "DPP", "DAP", "TPP", "TPA", "TAP", "Tid enkel", "Tid dubbel",
+    "Tid trippel", "Vila", "Kompisar", "Pappans vänner", "Nils vänner", "Nils familj",
+    "DT tid per man", "Antal varv", "Älskar med", "Sover med", "Nils sex",
+    "Prenumeranter", "Intäkt ($)", "Kvinnans lön ($)", "Mäns lön ($)", "Kompisars lön ($)",
+    "DT total tid (sek)", "Total tid (sek)", "Total tid (h)", "Minuter per kille"
 ]
 
-
 def säkerställ_kolumner(df):
-    for col in COLUMNS:
-        if col not in df.columns:
-            df[col] = ""
+    for kolumn in COLUMNS:
+        if kolumn not in df.columns:
+            df[kolumn] = ""
     return df[COLUMNS]
