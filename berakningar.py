8928e139a1743fde0566c635261fa2856a224106
mangebange76 <magnusberg47@gmail.com>
2025-07-31 07:16:01 +0200

    Update berakningar.py

--- berakningar.py
+++ berakningar.py
@@ -1,102 +1,85 @@
-def beräkna_radvärden(f):
-    # Grundvärden
-    c = f["Män"]
-    d = f["Fitta"]
-    e = f["Rumpa"]
-    f_dp = f["DP"]
-    g = f["DPP"]
-    h = f["DAP"]
-    i = f["TAP"]
-    j = f["Tid S"]
-    k = f["Tid D"]
-    l = f["Vila"]
-    s = f["Älskar"]
-    t = f["Sover med"]
-    v = f["Pappans vänner"]
-    w = f["Grannar"]
-    x = f["Nils vänner"]
-    y = f["Nils familj"]
-    z = c if c != 0 else 1  # undvik delning med noll
-    an = (c + d + e + f_dp + g + h + i)
-    ålder = f["Ålder"]
+def beräkna_radvärden(data):
+    c = data["Män"]
+    d = data["Fitta"]
+    e = data["Rumpa"]
+    f = data["DP"]
+    g = data["DPP"]
+    h = data["DAP"]
+    i = data["TAP"]
+    j = data["Tid S"]
+    k = data["Tid D"]
+    l = data["Vila"]
+    v = data["Pappans vänner"]
+    w = data["Grannar"]
+    x = data["Nils vänner"]
+    y = data["Nils familj"]
+    s = data["Älskar"]
+    t = data["Sover med"]
+    ab = data["Nils"]
 
-    # Beräkningar
-    m = (c + d + e) * j                       # Summa S
-    n = (f_dp + g + h) * k                    # Summa D
-    o = i * k                                 # Summa TP
-    p = (c + d + e + f_dp + g + h + i) * l    # Summa Vila
-    q = (m + n + o + p) / 3600                # Summa tid
-    r = 7 + 3 + q + 1                         # Klockan
-    u = v + w + x + y                         # Känner
-    aa = ((m / z) + (n / z) + (n / z) + (o / z) + (o / z) + (o / z) + 0) / 60  # Tid kille
-    ac = 10800 / z                            # Hångel
-    ad = (n * 0.65) / z                       # Suger
-    ae = an                                   # Prenumeranter (30 dagar hanteras i framtiden)
-    af = 15                                   # Avgift
-    ag = ae * af                              # Intäkter
-    ah = c * 120                              # Intäkt män
-    ai = (120 + f.get("Nils", 0)) * u         # Intäkt känner
-    aj = max(150, min(800, int(ae * 0.10)))   # Lön Malin
-    # Åldersjustering av Malins lön
-    if ålder < 18:
-        aj = aj * 0.8
-    elif ålder > 45:
-        aj = aj * 0.7
-    ak = ag * 0.20                            # Företagets intäkt
-    al = ag - ah - ai - aj - ak              # Vinst
-    am = u                                    # Känner Sammanlagt
-    an_hårdhet = 0
-    if f_dp > 0:
-        an_hårdhet += 2
+    # Summeringar
+    m = (c + d + e) * j
+    n = (f + g + h) * k
+    o = i * k
+    p = (c + d + e + f + g + h + i) * l
+    q = (m + n + o + p) / 3600  # timmar
+    r = 7 + 3 + q + 1           # klockan
+    u = v + w + x + y
+    z = u + c
+
+    try:
+        ac = 10800 / c if c > 0 else 0  # Hångel
+    except:
+        ac = 0
+
+    try:
+        ad = (n * 0.65) / z if z > 0 else 0  # Suger
+    except:
+        ad = 0
+
+    an = 1  # Prenumeranter per manuell enhet, justerbar
+    ae = (c + d + e + f + g + h + i) * an
+    af = 15
+    ag = ae * af
+    ah = c * 120
+    aj = max(150, min(800, ae * 0.10))  # Malins lön
+    ai = (aj + 120) * u
+    ak = ag * 0.20
+    al = ag - ah - ai - aj - ak
+    am = u
+
+    # Hårdhetspoäng
+    hårdhet = 0
+    if f > 0:
+        hårdhet += 2
     if g > 0:
-        an_hårdhet += 3
+        hårdhet += 3
     if h > 0:
-        an_hårdhet += 5
+        hårdhet += 5
     if i > 0:
-        an_hårdhet += 7
+        hårdhet += 7
 
-    # Sammansatt rad
-    rad = {
-        "Veckodag": f["Veckodag"],
-        "Scen": f["Scen"],
-        "Män": c,
-        "Fitta": d,
-        "Rumpa": e,
-        "DP": f_dp,
-        "DPP": g,
-        "DAP": h,
-        "TAP": i,
-        "Tid S": j,
-        "Tid D": k,
-        "Vila": l,
+    return {
+        **data,
         "Summa S": m,
         "Summa D": n,
         "Summa TP": o,
         "Summa Vila": p,
         "Summa tid": q,
-        "Klockan": round(r, 2),
-        "Älskar": s,
-        "Sover med": t,
+        "Klockan": r,
         "Känner": u,
-        "Pappans vänner": v,
-        "Grannar": w,
-        "Nils vänner": x,
-        "Nils familj": y,
-        "Totalt Män": u + c,
-        "Tid kille": round(aa, 2),
-        "Nils": f.get("Nils", 0),
-        "Hångel": round(ac, 2),
-        "Suger": round(ad, 2),
+        "Totalt Män": z,
+        "Tid kille": ((m / z) + (n / z) + (o / z) + ad) / 60 if z > 0 else 0,
+        "Hångel": ac,
+        "Suger": ad,
         "Prenumeranter": ae,
         "Avgift": af,
         "Intäkter": ag,
         "Intäkt män": ah,
         "Intäkt Känner": ai,
-        "Lön Malin": round(aj, 2),
-        "intäkt Företaget": round(ak, 2),
-        "Vinst": round(al, 2),
+        "Lön Malin": aj,
+        "intäkt Företaget": ak,
+        "Vinst": al,
         "Känner Sammanlagt": am,
-        "Hårdhet": an_hårdhet
+        "Hårdhet": hårdhet
     }
-
-    return rad
